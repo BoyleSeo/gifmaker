c184bb36f5132ce2d35760309c538b26da534fde
Boyle Seo <ozorion@naver.com>
2020-11-20 19:51:45 +0900

    🎈파일 인풋

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
- const ffmpeg = createFFmpeg({ log:true });
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+const ffmpeg = createFFmpeg({ log: true });
 
 function App() {
-  const [ready,setReady] = useState(false);
+  const [ready, setReady] = useState(false);
+  const [video, setVideo] = useState();
 
   //비동기처리
   const load = async () => {
     await ffmpeg.load(); //ffmpeg load promise를 받으면 ready를 true로
     setReady(true);
-  }
-  useEffect( () => {
+  };
+  useEffect(() => {
     load();
-  }, [] ) // '[]' ONLY RUN ONCE 
+  }, []); // '[]' ONLY RUN ONCE
 
   return ready ? (
     <div className="App">
-
+      <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
     </div>
-  ) : (<p> L O A D I N G . . . </p>)
+  ) : (
+    <p> L O A D I N G . . . </p>
+  );
 }
 
 export default App;
